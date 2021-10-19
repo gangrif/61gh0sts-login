b980861fc7ac06ab246a8cebcccb75e75092da7a
gangrif <war@undrground.org>
2021-10-18 22:50:37 -0400

    initial commit, login page is functional, but needs work.. TODO replace hardcoded but secure password with something more dynamic

---
+++ admin.php
@@ -0,0 +1,29 @@
+<!--
+        Written by ghouly gangrif
+        Always open, and available at github.somethingorother
+-->
+<html>
+	<head>
+		<title>s00per seekr3t DC61gh0sts adm1n page!</title>
+	</head>
+	<body bgcolor='#000000' text='#449977'>
+		<center><a href='/index.html'><-</a></a>-[<a href='about.html'>About DC61gh0sts</a>]-[<a href='https://crawl.defcon610.org'>Pubcrawl</a>]-</center>
+		<br>
+		<center>
+<?php
+if ($_POST && ($_POST['user'] == '61gh0sts' && base64_encode($_POST['passwd']) == 'Z2hvc3RzIGFyZSBwZW9wbGUgdG9v')) {
+	$flag = file_get_contents('/tmp/f');
+	echo "Welcome<br>$flag";
+} else {
+		echo "
+		<form action=admin.php method=post>
+			Username: <input type=text name=user><br>
+			Password: <input type=password name=passwd><br>
+			<input type=submit>
+		</form>
+		<br>";
+}
+?>
+		</center>
+	</body>
+</html>
